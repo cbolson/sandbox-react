d62d8ce294c727f04ad606982a30f57b27c153ca
cbolson <chris@cbolson.com>
2023-04-08 14:20:04 +0200

    complete turotial add README

---
+++ dot-track/README.md
@@ -0,0 +1,23 @@
+# React Dot Tracker
+
+## About
+
+Coding challenge to track mouse clicks on doc.
+
+## Information
+
+Coding Challenge Rules:
+
+1. Create a react application that allows users to click the screen and leave a dot (10px x 10px)
+2. create an "undo" button that allows users to remove the last dot.
+- it can be clicked as many times as needed. (until all dots are removed.)
+3. create a "redo" button that puts the dots back on the screen.
+- it can be clicked as many times as needed. (until all dots are restored.)
+
+## Added
+- ghost dots removed using undo
+- clear cache when new dot is added to avoid the "reset" button adding old dots back in after we have continued adding dots after removing others.
+
+## Sources
+
+- Tutorial [The Dev Evniroment](https://www.youtube.com/watch?v=D36HU7LpTCk&t=532s)
--- dot-track/src/App.tsx
+++ dot-track/src/App.tsx
@@ -6,24 +6,61 @@ interface Dots {
 }
 function App() {
   const [dots, setDots] = useState<Dots[]>([]);
-  const draw = (e: MouseEvent) => {
+  const [cacheDots, setCacheDots] = useState<Dots[]>([]);
+
+  const drawDot = (e: MouseEvent) => {
     const { clientX, clientY } = e;
+    // clear cached dots to avoid undo and redo messing it up
+
+    setCacheDots([]);
     setDots([...dots, { x: clientX, y: clientY }]);
   };
-  const undo = () => {
-    console.log("undo", dots);
+  const undoDot = () => {
+    if (dots.length > 0) {
+      // save copy of dots to be able to roll back
+      const newDots = [...dots];
+      // get last dot
+      const lastDot = newDots.pop() as Dots;
+      // save new dot arrays to state
+      Promise.all([
+        // add last dot to cache arr
+        setCacheDots([...cacheDots, lastDot]),
+        // re-create dots array with copied version minus the last dot
+        setDots(newDots),
+      ]);
+    }
+  };
+  const redoDot = () => {
+    if (cacheDots.length > 0) {
+      // create new cache array from current cache before it is modified
+      const newCache = [...cacheDots];
+      // get last cache item and remove
+      const lastCacheItem = newCache.pop() as Dot;
+      Promise.all([
+        // set cache array with cache minis the last dot
+        setCacheDots(newCache),
+        // add last dot back onto dots array
+        setDots([...dots, lastCacheItem]),
+      ]);
+    }
   };
   return (
     <div className='App'>
       <div className='controls'>
-        <button onClick={undo}>Undo</button>
-        <button>Reset</button>
+        <button onClick={undoDot}>Undo</button>
+        <button onClick={redoDot}>Reset</button>
       </div>
-      <div className='click-area' onClick={draw}>
+      <div className='click-area' onClick={drawDot}>
         {dots.map(({ x, y }: Dots, i: number) => (
           <div
             className='dot'
             key={`dot-${i}`}
+            style={{ left: x, top: y }}></div>
+        ))}
+        {cacheDots.map(({ x, y }: Dots, i: number) => (
+          <div
+            className='dot cache'
+            key={`cache-${i}`}
             style={{ left: x, top: y }}></div>
         ))}
       </div>
