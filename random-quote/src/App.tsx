d29227a7068add9b9427ba92481828b17da173fc
cbolson <chris@cbolson.com>
2023-04-11 21:13:40 +0200

    modification - quote component

--- random-quote/src/App.tsx
+++ random-quote/src/App.tsx
@@ -1,33 +1,11 @@
-import { useState, useEffect } from "react";
-import "./App.css";
 import Quote from "./components/Quote";
 
-function App() {
-  const [quotes, setQuotes] = useState([]);
-  const [curQuote, setCurQuote] = useState(null);
+import "./App.css";
 
-  // fetch quotes from the api on initial load
-  useEffect(() => {
-    (async () => {
-      console.log("load data");
-      const res = await fetch("https://type.fit/api/quotes");
-      const data = await res.json();
-      setQuotes(data);
-      setCurQuote(data[0]);
-    })();
-  }, []);
-  // get random quote
-  function getRandomQuote() {
-    const length = quotes.length;
-    const random = Math.floor(Math.random() * length + 1);
-    setCurQuote(quotes[random]);
-  }
+function App() {
   return (
     <div className='App'>
-      <button className='btn' onClick={getRandomQuote}>
-        New quote
-      </button>
-      <Quote quote={curQuote} />
+      <Quote />
     </div>
   );
 }
--- random-quote/src/components/Quote.tsx
+++ random-quote/src/components/Quote.tsx
@@ -1,15 +1,46 @@
+import { useState, useEffect } from "react";
+
 interface quoteProps {
   quote: {
     text: string;
     author: string;
   };
 }
+
 function Quote({ quote }: quoteProps) {
+  const [quotes, setQuotes] = useState([]);
+  const [curQuote, setCurQuote] = useState(null);
+
+  // fetch quotes from the api on initial load
+  useEffect(() => {
+    (async () => {
+      console.log("load data");
+      const res = await fetch("https://type.fit/api/quotes");
+      const data = await res.json();
+      setQuotes(data);
+      setCurQuote(data[0]);
+    })();
+  }, []);
+
+  // get random quote
+  function getRandomQuote() {
+    const length = quotes.length;
+    const random = Math.floor(Math.random() * length + 1);
+    setCurQuote(quotes[random]);
+  }
+
   return (
-    <div className='quote'>
-      {quote?.text}
-      <div className='author'>- {quote?.author}</div>
-    </div>
+    <>
+      <button className='btn' onClick={getRandomQuote}>
+        New quote
+      </button>
+
+      <div className='quote'>
+        {curQuote?.text}
+        <div className='author'>- {curQuote?.author}</div>
+      </div>
+    </>
   );
 }
+
 export default Quote;
