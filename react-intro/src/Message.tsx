dc50ac4843cd6a8cbfc0cb7c5d93daed73775236
Chris Bolson <chris@cbolson.com>
2023-04-11 10:11:16 +0200

    Delete react-intro directory

--- react-intro/src/Message.tsx
+++
@@ -1,7 +0,0 @@
-// PascalCasing - capatilize first letter of each word
-function Message() {
-  const name = "Chris";
-  if (name) return <h1>Hello {name}</h1>;
-  else return <h1>Hello world!</h1>;
-}
-export default Message;
