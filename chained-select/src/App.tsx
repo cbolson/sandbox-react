5d4501d380a273b6f12e65f39e0b86ba17a554be
cbolson <chris@cbolson.com>
2023-04-17 10:45:30 +0200

    New project - Chained Select

---
+++ chained-select/src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Locations from "./data/Locations";
+
+import "./App.css";
+interface propTypes {
+  Locations: Object;
+}
+function App() {
+  const [country, setCountry] = useState(Object.keys(Locations)[0]);
+
+  return (
+    <div className='App'>
+      <h1>Chained Country Select</h1>
+      <select onChange={(e) => setCountry(e.target.value)}>
+        {Object.keys(Locations).map((co, i: number) => (
+          <option key={`country-${i}`} value={co}>
+            {co}
+          </option>
+        ))}
+      </select>
+      <select>
+        {Locations[country].map((city: string, i: number) => (
+          <option key={`country-${i}`} value={city}>
+            {city}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default App;
