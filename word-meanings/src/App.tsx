07ca8600f453a81e2a1a34fd2728d0890c21aedf
cbolson <chris@cbolson.com>
2023-05-06 19:27:14 +0200

    add search box

--- word-meanings/src/App.tsx
+++ word-meanings/src/App.tsx
@@ -2,25 +2,41 @@ import axios from "axios";
 import "./App.css";
 import { useEffect, useState } from "react";
 
+const URL_API = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
 function App() {
   const [fetchedData, setFetchedData] = useState([]);
+  const [word, setWord] = useState<string>("");
 
-  const urlApi = "https://api.dictionaryapi.dev/api/v2/entries/en/hello";
+  // process word data retrieved via api
   useEffect(() => {
+    console.log(fetchedData);
+  }, [fetchedData]);
+
+  // call api only when user submits search
+  function buttonHandler() {
+    const urlApi = URL_API + word;
     const getData = async () => {
-      const data = await axios.get(urlApi);
-      setFetchedData(data.data);
+      const result = await axios.get(urlApi);
+      setFetchedData(result.data);
     };
     getData();
-  }, []);
-
-  console.log("data: ", fetchedData);
-
+  }
   return (
     <>
       <h1>Words and their meanings</h1>
-      <div>Searchbox</div>
-      <div>Word definitions etc.</div>
+      <section className='search-box'>
+        <input
+          type='text'
+          onChange={(e) => setWord(e.target.value)}
+          value={word}
+        />
+        <button onClick={buttonHandler} di sabled={word.length < 1 && true}>
+          search
+        </button>
+      </section>
+
+      <section className='definition'>data to show here....</section>
     </>
   );
 }
